091eb52669635cda8c0a1fe2830de620e230b965
Waxiw92 <63607294+Waxiw92@users.noreply.github.com>
2021-04-03 18:33:42 +0300

    Added Header (Logo, Statefull, UserInfo)

---
+++ src/components/AuthNav/AuthNav.js
@@ -0,0 +1,21 @@
+import React from "react";
+import { NavLink } from "react-router-dom";
+import styles from './AuthNav.module.css'
+
+
+export default function AuthNav() {
+  return (
+    <div>
+
+      <NavLink
+        to="/contacts"
+        exact
+        className={styles.authLink}
+        activeClassName={styles.authActiveLink}
+      >
+        Контакты
+      </NavLink>
+
+    </div>
+  );
+}
