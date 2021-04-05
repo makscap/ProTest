96a12f677e4ebb39f31edd4f75b2062f8ca613d8
AlexandrSybirko <63520677+AlexandrSybirko@users.noreply.github.com>
2021-04-05 20:16:09 +0300

    fixes

--- src/images/index.js
+++ src/images/index.js
@@ -1,34 +1,74 @@
-import Im1 from './Photo280.png';
-import Im2 from './logo.png';
-
+import Nikolai1 from './Nikolai270.jpg';
+import Nikolai2 from './Nikolai354.jpg';
+import Nikolai3 from './Nikolai450.jpg';
+import Andrei1 from './Andrei270.jpg';
+import Andrei2 from './Andrei354.jpg';
+import Andrei3 from './Andrei450.jpg';
 
   const images = {
-      id1: Im1
+      id1: {
+          sm: Nikolai1,
+          xs: Nikolai2,
+          xl: Nikolai3
+      },
+      id2: {
+          sm: Andrei1,
+          xs: Andrei2,
+          xl: Andrei3
+      },
+      id3: {
+          sm: Andrei1,
+          xs: Andrei2,
+          xl: Andrei3
+      },
+      id4: {
+          sm: Andrei1,
+          xs: Andrei2,
+          xl: Andrei3
+      },
+      id5: {
+          sm: Andrei1,
+          xs: Andrei2,
+          xl: Andrei3
+      },
+      id6: {
+          sm: Andrei1,
+          xs: Andrei2,
+          xl: Andrei3
+      },
+      id7: {
+          sm: Andrei1,
+          xs: Andrei2,
+          xl: Andrei3
+      },
+      id8: {
+          sm: Andrei1,
+          xs: Andrei2,
+          xl: Andrei3
+      }
 }
 
 const intViewportWidth = window.innerWidth
 
-console.log(intViewportWidth)
+const getViewport = () => {
 
-
-function currentImages (id) {
-    
-
-     if (intViewportWidth > 350 && intViewportWidth < 767) {
+    if (intViewportWidth > 320 && intViewportWidth < 768) {
         
-        return images[id] =  Im2
+         return 'sm'
     }
-    if (intViewportWidth > 768) {
-          return images[id] = Im1
+    if (intViewportWidth > 768 && intViewportWidth < 1280) {
+          return 'xs'
     }
-    if (intViewportWidth => 1280) {
-         return images[id] = Im1
+    if (intViewportWidth > 1280) {
+         return 'xl'
     }
-    
-    
 }
 
-console.log(currentImages())
+const currentImages = id => {
+    return images[id][getViewport()]
+}
+
+
 
 export default currentImages
 
