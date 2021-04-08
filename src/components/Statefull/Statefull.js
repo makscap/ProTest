23d315614c0b8ce616a88ab5484803e33410701c
IvannaMelnyk <givmiller@gmail.com>
2021-04-08 19:11:43 +0300

    info.json

--- src/components/Statefull/Statefull.js
+++ src/components/Statefull/Statefull.js
@@ -1,8 +1,8 @@
-import { useSelector } from "react-redux";
-import Navigation from "../Navigation/Navigation";
-import UserInfo from "../UserInfo/UserInfo";
-import AuthNav from "../AuthNav/AuthNav";
-import { authSelectors } from "../redux/auth";
+import { useSelector } from 'react-redux';
+import Navigation from '../Navigation/Navigation';
+import UserInfo from '../UserInfo/UserInfo';
+import AuthNav from '../AuthNav/AuthNav';
+import { authSelectors } from '../redux/auth';
 import s from './Statefull.module.css';
 
 export default function AppBar() {
