e4e127935ebf18de0494fe1a631772d49934e22b
Viktoriia Melanchenko <victoriamelanchenko@gmail.com>
2021-04-10 23:47:33 +0300

    add error messages

---
+++ src/components/AuthPage/Errors.js
@@ -0,0 +1,7 @@
+const userErrorMessages = {
+  ERROR_AUTHENTICATION: 'Error. Invalid email or password. Please try again.',
+  // FIELDS_REQUIRED: 'Email and password fields are required',
+  AUTH_FAILED: 'Authentication failed. Please try again.',
+};
+
+export default userErrorMessages;
