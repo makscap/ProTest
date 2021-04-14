7d3a520c6bfed40122205567b3bc0f909684184d
Viktoriia Melanchenko <victoriamelanchenko@gmail.com>
2021-04-14 21:03:05 +0300

    updated AuthForm to fix login

--- src/components/GoogleAuth/GoogleAuth.js
+++ src/components/GoogleAuth/GoogleAuth.js
@@ -1,26 +1,16 @@
 import React from 'react';
 import { FcGoogle } from 'react-icons/fc';
-import { useDispatch } from 'react-redux';
-import authOperations from '../redux/auth/auth-operations';
 
 import styles from './GoogleAuth.module.css';
 
 const GoogleAuth = () => {
-  const dispatch = useDispatch();
-
-  const googleLogIn = () => {
-    window.location.href = 'http://protest-api.herokuapp.com/api/users/google';
-
-    dispatch(authOperations.googleIn());
-  };
-
   return (
-    <button className={styles.button__google} onClick={googleLogIn}>
+    <a href="http://protest-api.herokuapp.com/api/users/google">
       <div className={styles.google_container}>
         <FcGoogle className={styles.google_icon} />
         Google
       </div>
-    </button>
+    </a>
   );
 };
 
